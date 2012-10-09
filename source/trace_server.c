692c2f4c08c3294392bf71098db3b4f3132d1054
susmit <susmit@cs.colostate.edu>
2012-10-09 11:37:15 -0600

    adding timeout

--- source/trace_server.c
+++ source/trace_server.c
@@ -680,7 +680,7 @@ static void *get_fwd_reply(void *arguments)
         timeout_ms *= 2;
     }
 
-    printf("Timeout %d\n", timeout);
+    printf("Timeout %d\n", timeout_ms);
     
 
     //express interest
